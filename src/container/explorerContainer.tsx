eda67159c752962e3cef108c8c3d613c8060733f
Daniel Sun <hjdaniel.sun@gmail.com>
2018-10-21 15:08:13 +1100

    added error handling when api request fails.

--- src/container/explorerContainer.tsx
+++ src/container/explorerContainer.tsx
@@ -17,7 +17,8 @@ import {fileSizeConverter, countFiles, countFileSize} from '../utils/utils';
 interface IExplorerState {
     data: IDataItem[],
     totalNumOfFiles: number,
-    totalFileSize: number
+    totalFileSize: number,
+    error: string
 }
 
 class ExplorerContainer extends React.Component<{}, IExplorerState> {
@@ -26,11 +27,17 @@ class ExplorerContainer extends React.Component<{}, IExplorerState> {
         this.state = {
             data: [],
             totalNumOfFiles: 0,
-            totalFileSize: 0
+            totalFileSize: 0,
+            error: ''
         };
     }
 
     public render() {
+
+        if (this.state.error !== '') {
+            return <div>{this.state.error}</div>;
+        }
+
         return <div className={'root'}>
             <div className='explorerMain'>
                 {this.state.data.length > 0 ? this.state.data.map((data: IDataItem, index) => (<FolderContainer key={index} name={data.name} children={data.children} type={data.type} size={data.size} />)): <CircularProgress />}
@@ -49,10 +56,15 @@ class ExplorerContainer extends React.Component<{}, IExplorerState> {
             headers: {'content-type': 'application/json'}
         });
 
+        if (!response.ok) {
+            this.setState({error: 'something is wrong'});
+            return;
+        }
+
         const data: IDataItem[] = (await response.json()).data as IDataItem[];
         const totalNumOfFiles: number = countFiles(data);
         const totalFileSize: number = countFileSize(data);
-        this.setState({data, totalFileSize, totalNumOfFiles});
+        this.setState({data, totalFileSize, totalNumOfFiles, error: ''});
     }
 }
 
